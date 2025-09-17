190cfb6e802d800283e7713414c0404e41d9064d
NotArmin <armineghtesadi@gmail.com>
2025-09-17 13:34:41 +0200

    Assignment 4

--- pointers.c
+++ pointers.c
@@ -11,6 +11,22 @@
 
 char* text1 = "This is a string.";
 char* text2 = "Yet another thing.";
+int counter = 0;
+
+int list1[20];
+int list2[20];
+
+void copycodes(const char* text, int* list, int* count) {
+  while (*text != '\0') {
+    *list++ = *text++;
+    (*count)++;
+  }
+}
+
+void work() {
+  copycodes(text1, list1, counter);
+  copycodes(text2, list2, counter);
+}
 
 void printlist(const int* lst){
   printf("ASCII codes and corresponding characters.\n");
@@ -38,3 +54,33 @@ int main(void){
 
     endian_proof((char*) &counter);
 }
+
+// Explain how you get the pointer adresses to the two char arrays (text1 and text2)
+// and the counter variable (count) in work()
+
+// text1, text2 and counter are global variables and can be accessed inside any function in the file
+
+// What does it mean to increment a pointer? What is the difference between incrementing a pointer to a 
+// ASCII text string and incrementing a pointer to an integer array? How is the ASM code different to C?
+
+// Incrementing a pointer means moving the pointer to the next memory location based on the data type it points to
+// For a char pointer, incrementing it moves to the next byte (1 byte)
+// For an int pointer, incrementing it moves to the next integer (typically 4 bytes on most systems)
+// In ASM, the increment operation will differ based on the size of the data type and will use different instructions
+
+// What is the difference between incrementing a pointer and incrementing a variable that a pointer points to?
+// Explain how your code increments the variable "count"
+
+// In line 22, in order to increment the variable "count" we dereference the pointer and then increment
+
+// Explain a statement in your code where you are dereferencing a pointer. What does this mean?
+// Compare it to the ASM code
+
+// As stated before, we dereference the pointer in line 22 to modify the value it points to. 
+// This means we are not modifying the memory adress the pointer holds, but the value at that adress
+// In ASM this is done by loading, modifying and then storing the value back
+
+// Is your computer using big-endian or little-endian representation? How did you come to your conclusion?
+// Is there any benefit of using one representation over the other?
+
+// My computer is using little-endian representation, since the least significant byte is stored first
